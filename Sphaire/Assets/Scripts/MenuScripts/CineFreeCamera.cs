04afd4bff4861d0a8ff251af81f7b35c4bdc732d
Jacob Zackaria <zackariajacob@gmail.com>
2019-01-05 13:35:02 +0530

    Added Cinemachine Camera
    Replaced Existing Camera with Cinemachine

---
+++ Sphaire/Assets/Scripts/MenuScripts/CineFreeCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CineFreeCamera : MonoBehaviour {
+
+	private CinemachineFreeLook freeLookCam;
+	private float sensitivityX = 3.0f;
+    private float sensitivityY = 0.01f;
+	public Joystick joystick;
+
+	void Start () {
+		freeLookCam = GetComponent<CinemachineFreeLook>();
+	}
+	
+	void Update () {
+		freeLookCam.m_XAxis.Value += (joystick.Horizontal * sensitivityX);
+		freeLookCam.m_YAxis.Value += (joystick.Vertical * sensitivityY);
+	}
+}
