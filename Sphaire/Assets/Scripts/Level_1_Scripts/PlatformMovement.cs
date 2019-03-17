52130180364af568bf2eed56209a4d93f6ba6130
Jacob Zackaria <zackariajacob@gmail.com>
2019-03-17 20:12:15 +0530

    Underworld Cont.

---
+++ Sphaire/Assets/Scripts/Level_1_Scripts/PlatformMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public Vector3 moveDirection;
+    public Vector3 moveSpeed;
+
+    private void Update() {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+}
