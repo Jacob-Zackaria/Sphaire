94fd9a4ecbded4067a906ded69e56c6b44a565c6
Jacob Zackaria <zackariajacob@gmail.com>
2019-03-26 21:40:30 +0530

    enemy animation controll

---
+++ Sphaire/Assets/Scripts/Level_1_Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    const float animationSmoothTime = 0.1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();    
+    }
+
+    void Update()
+    {
+        //Enemy forward and backward movememnt animation control.
+        float speedPercent = agent.velocity.magnitude / agent.speed; 
+        animator.SetFloat("Speed", speedPercent, animationSmoothTime, Time.deltaTime);
+
+        //Enemy left and right movement animation control.
+        float rotationPercent = agent.angularSpeed / 1f; 
+        animator.SetFloat("Strafing", rotationPercent, animationSmoothTime, Time.deltaTime);
+    }
+}
