5531864e31b2b8b2374a120e225bb514ffdfb91e
GEORGE'S LAP <zackariajacob@gmail.com>
2019-04-26 00:47:56 +0530

    platform movement loop and one time

--- Sphaire/Assets/Scripts/Level_1_Scripts/PlatformMovement.cs
+++ Sphaire/Assets/Scripts/Level_1_Scripts/PlatformMovement.cs
@@ -2,10 +2,100 @@
 
 public class PlatformMovement : MonoBehaviour
 {
+    public enum MoveMode
+    {
+        oneShot,
+        loop,
+        pingPong
+    }
+
+    public bool isActive;
+    public MoveMode moveMode;
+
     public Vector3 moveDirection;
     public float moveSpeed;
+    public float waitTime;
+
+    private Vector3 _finalPosition;
+    private float _distance;
+
+    /*public Sound[] sounds;
+    public static AudioManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject); 
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+        }
+    }*/
+
+    private void Start()
+    {
+        _finalPosition = transform.position + moveDirection;
+        _distance = Vector3.Distance(transform.position, _finalPosition);
+    }
 
     private void Update() {
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        if (isActive)
+        {
+            switch (moveMode)
+            {
+                case MoveMode.oneShot:
+                    OneShot();
+                    break;
+                case MoveMode.loop:
+                    Loop();
+                    break;
+                case MoveMode.pingPong:
+                    PingPong();
+                    break;
+            }
+        }
+    }
+
+    //One shot platform movement.
+    private void OneShot()
+    {
+        if (_distance > 0.1f)
+        {
+            transform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime);
+            _distance = Vector3.Distance(transform.position, _finalPosition);
+        }
+        else
+        {
+            isActive = false;
+        }
+    }
+
+    //Looping platform movement.
+    private void Loop()
+    {
+        if (_distance > 0.1f)
+        {
+            transform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime);
+            _distance = Vector3.Distance(transform.position, _finalPosition);
+        }
+        else
+        {
+            moveDirection *= -1;
+            _finalPosition = transform.position + moveDirection;
+            _distance = Vector3.Distance(transform.position, _finalPosition);
+        }
+    }
+
+    //Ping Pong movement.
+    private void PingPong()
+    {
+
     }
 }
